ab3741265baa1a9cbd0354431b9e69b2a5742092
Ikhlas Bano <98226842+ikhlasafzaal@users.noreply.github.com>
2024-04-08 03:37:48 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# Command-line Todo Application (TypeScript & Node.js)
+
+This command-line application, built with TypeScript and Node.js, allows users to manage their todo list with basic operations such as adding tasks, reading tasks, updating tasks, and deleting tasks.
+
+## Features
+
+- **Add**: Add new tasks to the todo list.
+- **Read**: View the current todo list.
+- **Edit**: Update existing tasks in the todo list.
+- **Delete**: Remove tasks from the todo list.
+- **Exit**: Exit the application.
+
+## Prerequisites
+
+- Node.js installed on your machine.
+- npm (Node Package Manager) installed on your machine.
+
+## Installation
+
+To install the Todos CLI, use npm:
+
+```bash
+npm install -g @ikhlasbano/todos
+```
+
+## Usage
+
+To use the Todos CLI, run the following command:
+
+```bash
+todos
+```
+
+Follow the prompts to perform various operations such as adding, reading, updating, and deleting tasks.
+
+## Accessing the Package on NPM
+
+You can access the package [here](https://www.npmjs.com/package/@ikhlasbano/todos).
+
+
+Feel free to customize the README further to add any additional information or instructions you think would be helpful for users. Happy task management!
---
+++ index.ts
@@ -0,0 +1,126 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todos: any[] = [];
+let choicesCondition = true;
+
+while(choicesCondition){
+    let questionsChoices = await inquirer.prompt([
+        {
+            type: 'list',
+            name: "choices",
+            message:"What would you like to choose",
+            choices:["Add", "Read", "Edit", "Delete", "Exit"]
+        }
+    ]);
+
+    if(questionsChoices.choices === "Add"){
+        let condition = true;
+        while (condition) {
+            let questionsToDos = await inquirer.prompt([
+                {
+                    name: "firstQuestion",
+                    type: "input",
+                    message: "What would you like to Add in List?"
+                }
+            ]);
+            if (!questionsToDos.firstQuestion) {
+                console.log(chalk.bold.yellowBright("Empty Data Insertion Not Allowed!"));
+            } else {
+                let questionsToDosConfirmation = await inquirer.prompt([
+                    {
+                        name: "secondQuestion",
+                        type: "confirm",
+                        message: "Do you want to Add More or Not?",
+                        default: "true"
+                    }
+                ]);
+                todos.push(questionsToDos.firstQuestion);
+                console.log(`${todos}`);
+                condition = questionsToDosConfirmation.secondQuestion;
+            }
+        }
+    }
+    
+    else if (questionsChoices.choices === "Read") {
+        if (todos.length === 0) {
+            console.log(chalk.yellow('No tasks available.'));
+        } else {
+            console.log(chalk.bold.cyanBright("ToDo List:"));
+            for (let todo of todos) {
+                console.log(`- ${todo}`);
+            }
+        }
+    }
+    
+
+    else if (questionsChoices.choices === "Edit") {
+        if (todos.length === 0) {
+            console.log(chalk.yellow('No tasks to update.'));
+        } 
+        else {
+            let updateChoice = await inquirer.prompt([
+                {
+                    name: 'index',
+                    type: 'number',
+                    message: 'Enter the serial no of the task you want to update:',
+                }
+            ]);
+    
+            const updateIndex = updateChoice.index - 1;
+    
+            if (updateIndex >= 0 && updateIndex < todos.length) {
+                let updateChoice = await inquirer.prompt([
+                    {
+                        name: 'updatedTask',
+                        type: 'input',
+                        message: 'Enter the updated task:'
+                    }
+                ]);
+                todos[updateIndex] = updateChoice.updatedTask;
+                console.log(chalk.bgGreenBright.bold('\n \t Task Updated Successfully.\n'));
+            } else {
+                console.log(chalk.red.bold('Invalid index or task does not exist.'));
+            }
+        }
+    }
+    
+
+    else if(questionsChoices.choices === "Delete"){
+        if(todos.length === 0) {
+            console.log(chalk.yellow('No tasks to delete.'));
+        }
+        else{
+            let deleteChoice = await inquirer.prompt([
+                {
+                    name: 'index',
+                    type: 'number',
+                    message: 'Enter the index of the task you want to delete:',
+                }
+            ]);
+            const deleteIndex = deleteChoice.index - 1;
+
+            if (deleteIndex >= 0 && deleteIndex < todos.length) {
+                todos.splice(deleteIndex, 1);
+                console.log(chalk.bgRedBright.bold('\n \t Task Deleted Successfully.\n'));
+            } else {
+                console.log(chalk.red.bold('Invalid index or task does not exist.'));
+            }
+        }
+        
+    }
+
+    else if(questionsChoices.choices === "Exit"){
+        choicesCondition = false;
+        console.log(chalk.red.bold('Exited from Program...'));
+    }
+    
+}
+
+
+
+
+
+
